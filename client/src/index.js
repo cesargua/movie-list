2dbfc58919a3922a947a7ccdf65756943a66a08c
cesargua <cguajard@ucsd.edu>
2023-06-07 00:33:00 -0700

    amplify

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import Parse from './parse.js'
+import movies from './data/movieData.js'
 
 //import styles from '../dist/style.css'
 
 //put callback here
-Parse.readAll((data)=>{
-    var movies = [...JSON.parse(data)]
-    ReactDOM.render(<App movies={movies}/>, document.getElementById('app'));
-});
+// Parse.readAll((data)=>{
+//     var movies = [...JSON.parse(data)]
+//     ReactDOM.render(<App movies={movies}/>, document.getElementById('app'));
+// });
+
+ReactDOM.render(<App movies={movies}/>, document.getElementById('app'));
